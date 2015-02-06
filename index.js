6fd5fc12646a03ab44c41f3dd070e119be34120e
Mathias Buus <mathiasbuus@gmail.com>
2015-02-06 13:30:09 -0800

    simpler prefix handling

--- index.js
+++ index.js
@@ -4,16 +4,15 @@ var enumerator = function (db, opts) {
   if (!opts) opts = {}
 
   var sep = opts.sep || '!'
-  var prefix = opts.prefix ? sep + prefix + sep : ''
-  var keyPrefix = prefix + 'keys!'
-  var countKey = prefix + 'count'
+  var prefix = opts.prefix ? sep + opts.prefix + sep : ''
+  var keyPrefix = prefix + sep
 
   var count = 0
   var lock = mutexify()
 
   var getCount = function (cb) {
     if (count) return cb(null, count)
-    db.get(countKey, {valueEncoding: 'utf-8'}, function (err, cnt) {
+    db.get(keyPrefix, {valueEncoding: 'utf-8'}, function (err, cnt) {
       if (err && !err.notFound) return cb(err)
       count = Number(cnt || 0)
       cb(null, count)
@@ -33,7 +32,7 @@ var enumerator = function (db, opts) {
             value: count.toString()
           }, {
             type: 'put',
-            key: countKey,
+            key: keyPrefix,
             valueEncoding: 'utf-8',
             value: (count + 1).toString()
           }]
@@ -57,6 +56,8 @@ var enumerator = function (db, opts) {
   }
 
   return function (key, cb) {
+    if (!key) throw new Error('key is required')
+
     key = keyPrefix + key
 
     db.get(key, {valueEncoding: 'utf-8'}, function (err, num) {
